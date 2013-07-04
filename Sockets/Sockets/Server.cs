fa860ba3fd827314849cca6e8f4cf27299ff9125
PauloAmora <pap43@pitt.edu>
2013-07-03 23:05:16 -0300

    Commit inicial, classe servidor criada
    Nenhuma UI feita nem implementação terminada

---
+++ Sockets/Sockets/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    class Server
+    {
+        private Socket socket;
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
